70c7fc20d0801505679fa84dc16ec759ca1d00b0
Casey Kelly <casey.kelly@colorado.edu>
2017-07-07 21:21:51 -0600

    create function to take private key from command line argument and return address

--- keygen.js
+++ keygen.js
@@ -2,7 +2,13 @@ var EthUtil = require("ethereumjs-util")
 // console.log(`0:${process.argv[0]}\n1:${process.argv[1]}\n2:${process.argv[2]}`)
 
 var hexToBytes = function(hex) {
-  for (var bytes = [], c = 0; c < hex.length; c += 2)
-  bytes.push(parseInt(hex.subtr(c, 2), 16));
-  return bytes;
+    for (var bytes = [], c = 0; c < hex.length; c += 2)
+    bytes.push(parseInt(hex.substr(c, 2), 16));
+    return bytes;
 }
+
+var privateKeyToAddress = function(privateKey) {
+    return `0x${EthUtil.privateToAddress(hexToBytes(privateKey)).toString('hex')}`
+}
+
+console.log(privateKeyToAddress(process.argv[2]))
